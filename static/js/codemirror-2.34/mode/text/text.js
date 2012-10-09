7226c24d0825f161fb3e10af03752e439517c8e4
TJ Kells <tjkells@gmail.com>
2012-10-09 16:56:11 -0500

    add required token function

--- static/js/codemirror-2.34/mode/text/text.js
+++ static/js/codemirror-2.34/mode/text/text.js
@@ -1,5 +1,9 @@
-CodeMirror.defineMode("text", function(config, parserConfig) {
-	return {};
+CodeMirror.defineMode("mymode", function(config, parserConfig) {
+  return {
+    token: function(stream, state) {
+      return null;
+    }
+  };
 });
 
 CodeMirror.defineMIME("text/plain", "text");
